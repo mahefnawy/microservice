667c992545c5eef5731566d1ca9be6bebeba9a68
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-06-11 16:57:10 +0300

    some testing data

--- serverless.js
+++ serverless.js
@@ -2,21 +2,42 @@ const { Component, utils } = require('@serverless/core')
 
 class Microservice extends Component {
   async default(inputs = {}) {
-    // console.log(this.context.status)
-    this.context.status('Deploying')
+    this.context.log('Deployment Started')
+
+    this.state = {
+      foo: 'bar',
+      ...inputs
+    }
+
+    await this.save()
+
+    this.context.status('Packaging')
+
+    await utils.sleep(1000)
+
+    this.context.status('Uploading')
+
+    await utils.sleep(1000)
+
+    this.context.status('Updating')
+
+    await utils.sleep(1000)
+
+    this.context.status('Finalizing')
 
     await utils.sleep(1000)
 
     this.state = { foo: 'bar' }
 
-    this.context.log('this is a log')
-
     await utils.sleep(1000)
 
+    this.state = {
+      ...this.state,
+      arn: 'aws:lambda:abc:us-east-1:qwertyuiop'
+    }
     await this.save()
 
-    // throw Error('something bad happened')
-
+    this.context.log('Deployment Successful')
     this.context.log()
     this.context.output('name', `       hello-world`)
     this.context.output('description', `Hello World Function`)
@@ -24,7 +45,20 @@ class Microservice extends Component {
     this.context.output('timeout', `    10`)
     this.context.output('arn', `        aws:lambda:abc:us-east-1:qwertyuiop`)
 
-    return { arn: 'aws:lambda:abc:us-east-1:qwertyuiop' }
+    return { arn: this.state.arn }
+  }
+
+  async remove(inputs = {}) {
+    this.context.log('Removal Started')
+
+    this.context.status('Removing')
+
+    await utils.sleep(3000)
+
+    this.state = {}
+    await this.save()
+
+    return inputs
   }
 }
 
